35d31fbe2e0b46094f682f007e725f2fcd7fe135
samuj02 <samuel.jimenez8643@alumnos.udg.mx>
2023-09-06 21:41:21 -0600

    Agregué formulario en Views

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,6 +6,12 @@
     <title>Contacto</title>
 </head>
 <body>
-    
+    <h1>Formulario de contacto</h1>
+    <form method="post" action="">
+        <label for="nombre">Nombre: </label><input type="text" name="nombre"><br>
+        <label for="correo">Email: </label><input type="email" name="correo"><br>
+        <label for="comment">Mensaje: </label><textarea name="comment"></textarea><br>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
